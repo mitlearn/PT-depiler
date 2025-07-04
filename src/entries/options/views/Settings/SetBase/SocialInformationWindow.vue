9e54a9d44a5f543c4687d9fcfbe8e653396b12bd
mitlearn <64004444+mitlearn@users.noreply.github.com>
2025-07-05 00:04:00 +0800

    Update SocialInformationWindow.vue

--- src/entries/options/views/Settings/SetBase/SocialInformationWindow.vue
+++ src/entries/options/views/Settings/SetBase/SocialInformationWindow.vue
@@ -78,7 +78,7 @@ const configStore = useConfigStore();
         v-model="configStore.socialSiteInformation.socialSite!.themoviedb.apikey"
         :label="t('socialConfig.themoviedbApikey')"
         clearable
-        :messages="themoviedb"
+        :messages="themoviedb msg"
       >
         <template #prepend>
           <v-avatar image="/icons/social/anidb.png" />
