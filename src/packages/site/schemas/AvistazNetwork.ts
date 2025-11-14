a54fa4624525571323678445a8955f69f6bad08d
mitlearn <64004444+mitlearn@users.noreply.github.com>
2025-11-14 23:06:03 +0800

    Update AvistazNetwork.ts

--- src/packages/site/schemas/AvistazNetwork.ts
+++ src/packages/site/schemas/AvistazNetwork.ts
@@ -193,7 +193,7 @@ export const SchemaMetadata: Pick<
         ...commonListSelectors,
         rows: { selector: "div.card-body.p-2 > div.table-responsive > table > tbody > tr" },
 
-        title: { selector: "a[href^="/torrent/"]", attr: 'title' },
+        title: { selector: "mb-1 a[href*='/torrent/']", attr: "title" },
         link: { selector: "div.float-right a[href*='/download/torrent/']", attr: "href" },
         size: { selector: "span.text-yellow[data-original-title='File Size']", filters: [{ "name": "parseSize" }] },
 
